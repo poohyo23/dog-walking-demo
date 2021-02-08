c97cef1c7e8b189d6b67df2f9f88f85df10f51e0
2ezeeni <dpwls6787@gmail.com>
2021-02-08 20:22:55 +0900

    well on local

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,7 +8,7 @@ class Menu extends React.Component{
   render(){
     return <nav style={{background: 'white'}}>
     <ul>
-    <li><a href="/"><FontAwesomeIcon icon={faCloudSun} style={{width: 55, height:55, color: '#aeaeae'}} /></a></li>    
+    <li><a href="/dog-walking-demo/#"><FontAwesomeIcon icon={faCloudSun} style={{width: 55, height:55, color: '#aeaeae'}} /></a></li>    
     <li><a href="/dog-walking-demo/#/walktimer"><FontAwesomeIcon icon={faHourglassHalf} style={{width: 55, height:55, color: '#aeaeae'}} /></a></li>
     <li><a href="/dog-walking-demo/#/diary"><FontAwesomeIcon icon={faClipboard} style={{width: 55, height:55, color: '#aeaeae'}} /></a></li>        
     </ul>
